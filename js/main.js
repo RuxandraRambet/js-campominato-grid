a83b5ab7bd45b65d8241c818e8fba0ee4d594207
RuxandraRambet <ruxandraramb@gmail.com>
2023-11-16 21:18:02 +0100

    feat: esercizio dopo la correzione plus bonus

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
+
 const board = document.querySelector('.board');
-const playButton = document.querySelector('.play-btn'); 
 
 // funzione che crea elemento
 function myCreateElement(tag, className, content){
@@ -10,22 +10,61 @@ function myCreateElement(tag, className, content){
 
     return element;
 }
+ //numero di celle in funzione della scelta utente
+function setCellNumber(level){
+    let cellNumber;
+    switch(level){
+        case 'medium':
+            cellNumber = 81;
+            break;
 
-// Creazione elementi html
+        case 'hard':
+            cellNumber = 49;
+            break;
 
-for( let i = 1; i <= 100; i++){
-    const myElement = myCreateElement('div','cell', i);
-    myElement.addEventListener('click', function(){
-        console.log(i); //stampo il numero della cella cliccata
-        myElement.classList.add('active-cell') // cambio colore della cella cliccata
-    });
-    board.append(myElement);
-    
-}
-// Click al bottone
-
-playButton.addEventListener('click', function(){
-    if(board.classList.contains('hidden')){
-        board.classList.remove('hidden')
+        case 'easy':
+        default:
+            cellNumber = 100;
+            break;
     }
-});
+    return cellNumber;
+}
+
+function creatBoard (mainElement, cellNumber){
+    
+    
+    const fragment = document.createDocumentFragment();
+    const cells = Math.sqrt(cellNumber);
+
+    for( let i = 1; i <= cellNumber; i++){
+        const myElement = myCreateElement('div','cell', i);
+        myElement.classList.add(`cell-${cells}`);
+
+        myElement.addEventListener('click', function(){
+            console.log(i); //stampo il numero della cella cliccata
+            myElement.classList.add('active-cell'); // cambio colore della cella cliccata
+        });
+        fragment.append(myElement);
+    }
+    mainElement.append(fragment);
+}
+function campoMinato(){
+    resetFn()
+    
+    let level = document.getElementById('level');
+    let levelStatus = level.value; //sarà determinato dall'utente con la select html
+    const cellNumber = setCellNumber(levelStatus);
+    console.log(levelStatus);
+    console.log(cellNumber);
+
+    creatBoard(board, cellNumber);
+}
+// Funzione di reset
+function resetFn(){
+    board.innerHTML = "";
+}
+
+const playButton = document.querySelector('.play-btn'); 
+
+// Click al bottone compare la board
+playButton.addEventListener('click', campoMinato);
